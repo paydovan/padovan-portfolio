5b56bf0c60b67d4640063c4d370705fc8d49b770
padovan <jpmpadovan@gmail.com>
2024-09-25 14:48:41 -0300

    feat: adding flowbite component to see if the navbar works

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,45 +1,49 @@
-import { useState } from 'react'
 import logo from '../assets/logo.svg'
-import { FaBars } from 'react-icons/fa'
 
 export function Nav() {
-  const [isNavOpen, setIsNavOpen] = useState(false)
-
-  const toggleNavbar = () => {
-    setIsNavOpen(!isNavOpen)
-  }
-
   return (
     <header className="">
-      <nav id="nav">
+      <nav>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="."
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img src={logo} className="h-8" alt="Logo" />
+            <img src={logo} className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap">
               padovan.io
             </span>
           </a>
           <button
-            onClick={toggleNavbar}
-            onTouchStart={event => {
-              event.preventDefault()
-            }}
+            data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-700 cursor-pointer"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-default"
+            aria-expanded="false"
           >
-            <FaBars size={36} />
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
           </button>
-          <div
-            className={`${isNavOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}
-          >
-            <ul className="font-medium bg-gray-900 md:bg-transparent flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 gap-2 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li>
                 <a
                   href="."
-                  className="block py-2 px-3 hover:bg-gray-800 md:hover:bg-transparent rounded md:bg-transparent md:p-0"
+                  className="block py-2 px-3 text-white rounded bg-gray-800 md:bg-transparent md:p-0"
                   aria-current="page"
                 >
                   Home
@@ -48,7 +52,7 @@ export function Nav() {
               <li>
                 <a
                   href="#projects"
-                  className="block py-2 px-3 rounded hover:bg-gray-800 md:hover:bg-transparent md:border-0  md:p-0"
+                  className="block py-2 px-3 rounded bg-gray-800 md:bg-transparent md:border-0 md:p-0"
                 >
                   Projetos
                 </a>
@@ -56,7 +60,7 @@ export function Nav() {
               <li>
                 <a
                   href="#services"
-                  className="block py-2 px-3 rounded hover:bg-gray-800 md:hover:bg-transparent md:border-0  md:p-0"
+                  className="block py-2 px-3 rounded bg-gray-800 md:bg-transparent md:border-0 md:p-0"
                 >
                   Serviços
                 </a>
@@ -64,7 +68,7 @@ export function Nav() {
               <li>
                 <a
                   href="#about"
-                  className="block py-2 px-3 rounded hover:bg-gray-800 md:hover:bg-transparent md:border-0  md:p-0"
+                  className="block py-2 px-3 rounded bg-gray-800 md:bg-transparent md:border-0 md:p-0"
                 >
                   Sobre
                 </a>
